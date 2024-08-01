97e766bcfd7c2fe9b7cc68bf7c78a09f32d2b7da
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-08-01 23:51:07 +0545

    Create forloop.java

---
+++ college/forloop.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+ for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }     
+        }
+    }
+}
+
+//For each
+int[] intarr = new int[]{1,2,3,4,5,6,7,8};
+for (int i : intarr) {
+    System.out.println(i);
+
+}
