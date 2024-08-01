40aa73401dbc705e517c0e977769a16971098360
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-08-01 23:51:56 +0545

    Create switchcase.java

---
+++ college/switchcase.java
@@ -0,0 +1,23 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+     switch (a) {
+        case 1:
+            System.out.println("One");
+            break;
+        case 2:
+            System.out.println("Two");
+            break;
+        case 3:
+               System.out.println("Three");
+               break;
+        case 4:
+                  System.out.println("Four");
+                  break;
+              
+        default:
+        System.out.println("Enter from 1 to 4");
+            break;
+    }
+        }
+    }
+}
