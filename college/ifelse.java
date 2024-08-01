58f5cf44dbfd622da06868961ca6927cec137d53
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-08-01 23:49:53 +0545

    Update ifelse.java

--- college/ifelse.java
+++ college/ifelse.java
@@ -1,1 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        if (a==5) {
+            System.out.println("Hello World!!"); // COnsole writeline
+
+        }
+        else if(a==2){
+            System.out.println("Two");  //sout shortcut
+            
+        }
+        else if (a == 3){
+            System.out.println("Three");
+        }
+        else{
+            System.out.println("Enter 1 to 3");
+        }
+    }
+}
 
