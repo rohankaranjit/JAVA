20f2138fad8cb6898324241d35fca8bbf909fa12
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-08-02 23:27:32 +0545

    Update switchcase.java

--- college/switchcase.java
+++ college/switchcase.java
@@ -2,7 +2,7 @@ public class App {
 
 public static void main(String[] args) throws Exception {
      
-switch (a) {
+        switch (a) {
         case 1:
             System.out.println("One");
             break;
