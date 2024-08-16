b3855b03b7cd720911bd6d346c00961e90c57c60
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-08-16 23:22:14 +0545

    Update matrixsum.java

--- college/matrixsum.java
+++ college/matrixsum.java
@@ -1,4 +1,8 @@
 public class TwoD {
     public static void main(String[] args) throws Exception{
+        int[][] matrix = new int[][] {{1,2,3},{4,5,7},{8,9,5}};
+        
+        for (int i = 0; i < 3; i++) {
+            int sum = 0;
 
 
