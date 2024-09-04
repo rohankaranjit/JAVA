41c533988e20fc2ffbf3b550f488d8d0a3272c7e
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-09-04 22:21:18 +0545

    Update awt.java

--- college/awt.java
+++ college/awt.java
@@ -1,1 +1,16 @@
+import java.awt.*;
+
+public class awt {
+    awt() {
+        Frame fm = new Frame("Title");
+        Label lb = new Label("Welcome to Java Graphics");
+        fm.add(lb);
+        fm.setSize(300, 300);
+        fm.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new awt();
+    }
+}
 
