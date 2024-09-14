3ff5b097ec29b7d55ef4ee444e62311f67cc24d6
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-09-14 23:33:23 +0545

    Update and rename myServer.java to MyServer.java

---
+++ college/rmi/MyServer.java
@@ -0,0 +1,25 @@
+import java.rmi.*;
+import java.rmi.registry.*;
+import java.rmi.server.UnicastRemoteObject;
+public class MyServer extends UnicastRemoteObject implements Adder {
+    public MyServer() throws RemoteException
+    {
+        super();
+    }
+    
+    public static void main(String[] args) throws RemoteException{
+        try{
+            Registry reg = LocateRegistry.createRegistry(5000);
+            reg.rebind("hi_server", new MyServer());
+            System.out.println("Server is now Ready");
+        }
+        catch (RemoteException e)
+        {
+            System.out.println(e);
+        }
+    }
+    public int Add(int x , int y ) throws RemoteException{
+        return x + y;
+    }
+}
+
